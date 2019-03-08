cc8d6710ab38522741fd4c11829250bd0c62d732
SamDaltonJr <spd4nf@virginia.edu>
2019-03-08 16:40:55 -0600

    laying out components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import { Route, withRouter, Switch } from 'react-router-dom';
+
 import './App.css';
 
+import Home from './components/Home';
+import Matchup from './components/Matchup';
 import NavBar from './components/NavBar';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavBar/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				{/* <NavBar /> */}
+				<Switch>
+					<Route exact path='/home' component={Home} />
+					<Route exact path='/matchup' component={Matchup} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/components/Matchup/Matchup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Matchup extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className='team team-1'>
+					<h2>Team 1</h2>
+				</div>
+				<div className='team team-2'>
+					<h2>Team 2</h2>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
